040cfe3fc55a8145a92ea171d9f16b953c047f5a
Troy Dorman <troydorman@gmail.com>
2021-07-30 20:11:17 -0600

    wrote the changeImg() function then call it

--- main.js
+++ main.js
@@ -7,3 +7,17 @@ images[1] = "http://lorempixel.com/400/200/sports";
 images[2] = "http://lorempixel.com/400/200/food";
 images[3] = "http://lorempixel.com/400/200/people";
 
+//change image
+function changeImg() {
+  document.slide.src = images[i];
+
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+
+  setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
